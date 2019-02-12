e020a8ca1ef8bf0e76a3a5a5eddceccd35fa2013
Rafael Cunha Reis <rcunhare@everis.com>
2019-02-12 16:18:57 -0200

    Comentários input files

--- src/aura/InputFiles/InputFilesController.js
+++ src/aura/InputFiles/InputFilesController.js
@@ -3,8 +3,9 @@
         var inputCmp = event.getSource().get("v.files");
         var files = component.get("v.files");
         var fr = new FileReader();
-        var i = 0;
-        
+        var i = 0;//Contador para ler mais de um arquivo
+
+        /* Quando a leitura é completada, salva o arquivo no atributo */
         fr.onload = function (readerEvt) {
             var binaryString = readerEvt.target.result;
             var filesObj = new Object(); 
@@ -15,8 +16,9 @@
             files.push(filesObj);
             i++;
         };
-        
+        /* função feita ao final da leitura */
         fr.onloadend = function(){
+            /* Ler o próximo arquivo, se houver um */
             if( i <= inputCmp.length-1 ){
             	fr.readAsBinaryString(inputCmp[i]);
             }else{
@@ -26,6 +28,7 @@
         };
         
         component.set("v.spinner", true);
+        /* inicia a leitura do primeiro arquivo (Blob), assícronamente */
         fr.readAsBinaryString(inputCmp[i]);
     },
     saveFiles : function(component,event,helper){
